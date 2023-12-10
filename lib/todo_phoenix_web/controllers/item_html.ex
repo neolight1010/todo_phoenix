bcf1ee4e00d4d573975156c302fd208849f88a6d
Anthony Suárez <neolight1010@gmail.com>
2023-12-10 10:07:57 -0500

    Show real items on /items

--- lib/todo_phoenix_web/controllers/item_html.ex
+++ lib/todo_phoenix_web/controllers/item_html.ex
@@ -10,4 +10,11 @@ defmodule TodoPhoenixWeb.ItemHTML do
   attr :action, :string, required: true
 
   def item_form(assigns)
+
+  def complete(item) do
+    case item.status do
+      1 -> "completed"
+      _ -> ""
+    end
+  end
 end
